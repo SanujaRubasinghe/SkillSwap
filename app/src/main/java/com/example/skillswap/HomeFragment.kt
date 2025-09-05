36fed15be16cf94f29c57b1b5836d91aca230f2d
Sanuja Rubasinghe <sanujarubasinghe@Sanujas-MacBook-Air.local>
2025-09-05 11:27:19 +0530

    (fix) styling

--- app/src/main/java/com/example/skillswap/HomeFragment.kt
+++ app/src/main/java/com/example/skillswap/HomeFragment.kt
@@ -1,20 +1,28 @@
 package com.example.skillswap
 
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.button.MaterialButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val images = listOf(
+            R.drawable.pasta_mastery,
+            R.drawable.guitar_mastery
+        )
+
         val notificationBtn = view.findViewById<FloatingActionButton>(R.id.notification_btn)
         val searchBtn = view.findViewById<FloatingActionButton>(R.id.search_btn)
-        val userProfileBtn = view.findViewById<MaterialButton>(R.id.user_profile_btn)
+        val cardContainer = view.findViewById<LinearLayout>(R.id.lessonCardContainer)
 
         notificationBtn.setOnClickListener {
             val intent = Intent(requireContext(), NotificationActivity::class.java)
@@ -34,5 +42,31 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             )
         }
 
+        for (image in images) {
+            val cardView = CardView(requireContext()).apply {
+                layoutParams = LinearLayout.LayoutParams(
+                    200.dpToPx(),
+                    250.dpToPx()
+                ).apply {
+                    setMargins(10.dpToPx(), 0, 10.dpToPx(), 0)
+                }
+                radius = 20f
+            }
+
+            val imageView = ImageView(requireContext()).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                scaleType = ImageView.ScaleType.CENTER_CROP
+                setImageResource(image)
+            }
+
+            cardView.addView(imageView)
+            cardContainer.addView(cardView)
+        }
+
     }
+
+    fun Int.dpToPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
 }
